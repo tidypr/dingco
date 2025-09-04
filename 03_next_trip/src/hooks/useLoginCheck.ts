839c08f9a354998978dd3bdd371ea8753dcd36e5
tidypr <tidypr@icloud.com>
2025-09-04 17:53:23 +0900

    fix: build error

--- 03_next_trip/src/hooks/useLoginCheck.ts
+++ 03_next_trip/src/hooks/useLoginCheck.ts
@@ -1,10 +1,13 @@
+'use client';
+
 import { useRouter } from 'next/navigation';
 
 export const useLoginCheck = () => {
   const router = useRouter();
 
   const loginCheckFn = () => {
-    alert('로그인을 먼저 해 주세요!');
+    // CHECK: build error
+    // alert('로그인을 먼저 해 주세요!');
     router.push('/auth/login');
   };
 
